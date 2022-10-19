76176be40cd75fe96c8cc13fb7858fb782955eb2
TRANANHTHAO <103154241+TRANANHTHAO@users.noreply.github.com>
2022-10-19 18:29:16 +0700

    Xay dung trang tong quan cac san pham

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:myshop/ui/products/product_overview_screen.dart';
 import 'ui/products/products_manager.dart';
 import 'ui/products/product_detail_screen.dart';
+//import 'ui/products/products_overview_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,10 +24,8 @@ class MyApp extends StatelessWidget {
           secondary: Colors.deepOrange,
         ),
       ),
-      home: SafeArea(
-        child: ProductDetailScreen(
-          ProductsManager().items[0],
-        ),
+      home: const SafeArea(
+        child: ProductsOverviewScreen(),
       ),
     );
   }
