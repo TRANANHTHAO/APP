116802b36008a7c221b2e3acdffc3e7f9e99322b
TRANANHTHAO <103154241+TRANANHTHAO@users.noreply.github.com>
2022-10-19 19:02:45 +0700

    Xay dung trang gio hang

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:myshop/ui/products/product_overview_screen.dart';
 import 'ui/products/products_manager.dart';
 import 'ui/products/product_detail_screen.dart';
 import 'ui/products/user_products_screen.dart';
+
+import 'ui/cart/cart_screen.dart';
 //import 'ui/products/products_overview_screen.dart';
 
 void main() {
@@ -26,55 +28,8 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: const SafeArea(
-        child: UserProductsScreen(),
+        child: CartScreen(),
       ),
     );
   }
 }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
