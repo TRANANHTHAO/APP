b83ef67ac7df12a498ea9491cce45e6c017fb04a
TRANANHTHAO <103154241+TRANANHTHAO@users.noreply.github.com>
2022-10-19 20:13:26 +0700

    Quan ly trang thai voi provider - OrdersManager

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myshop/ui/cart/cart_manager.dart';
 import 'package:myshop/ui/products/product_overview_screen.dart';
+import 'package:myshop/ui/screens.dart';
 import 'ui/products/products_manager.dart';
 import 'ui/products/product_detail_screen.dart';
 import 'ui/products/user_products_screen.dart';
@@ -28,6 +29,9 @@ class MyApp extends StatelessWidget {
           ),
           ChangeNotifierProvider(
             create: (ctx) => CartManager(),
+          ),
+          ChangeNotifierProvider(
+            create: (ctx) => OrdersManager(),
           ),
         ],
         child: MaterialApp(
--- lib/ui/orders/orders_screen.dart
+++ lib/ui/orders/orders_screen.dart
@@ -4,6 +4,7 @@ import 'orders_manager.dart';
 import 'order_item_card.dart';
 
 import '../shared/app_drawer.dart';
+import 'package:provider/provider.dart';
 
 class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
@@ -12,15 +13,19 @@ class OrdersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print('building orders');
-    final ordersManager = OrdersManager();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Orders'),
       ),
       drawer: const AppDrawer(),
-      body: ListView.builder(
-        itemCount: ordersManager.orderCount,
-        itemBuilder: (ctx, i) => OrderItemCard(ordersManager.orders[i]),
+      body: Consumer<OrdersManager>(
+        builder: (ctx, ordersManager, child) {
+          return ListView.builder(
+            itemCount: ordersManager.orderCount,
+            itemBuilder: (ctx, i) => OrderItemCard(ordersManager.orders[i]),
+          );
+        },
       ),
     );
   }
